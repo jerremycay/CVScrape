cab2bc24c5ae5d6d7ece272527d6418f4c649fd3
jerremycay <jerremycay@gmail.com>
2011-09-11 20:56:25 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CVScrape::Application.config.secret_token = '346f60419474207937eb1cf60f39ca47a1435a635d76c81e116c598e70c28980522f5be5e79e9c5beb0e8f72ff213302309bb19df55a44fab33554cb26bf2675'
